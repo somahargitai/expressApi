a598d75cda299077826ba9a94c732a0461bce516
Soma Hargitai <hargitai.soma@gmail.com>
2019-12-16 13:16:28 +0100

    minor changes

--- server.js
+++ server.js
@@ -3,12 +3,10 @@ import express from 'express';
 import router from './router';
 
 const app = express();
-const port = 1987;
-
-const resp = 'port';
+const port = 5000;
 
 app.use(router);
 
 app.listen(port, () => {
-  console.log(port);
+  console.log(`connected on port ${port}`);
 });
