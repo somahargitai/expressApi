125656b685a7bbb033792a0d43260a79c77561e0
Soma Hargitai <hargitai.soma@gmail.com>
2019-12-17 14:09:27 +0100

    morgan init

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import morgan from 'morgan';
 
 import router from './router';
 
 const app = express();
 const port = 5000;
 
+app.use(morgan('dev'));
 app.use(router);
 
 app.listen(port, () => {
